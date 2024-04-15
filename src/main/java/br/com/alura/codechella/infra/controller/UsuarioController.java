2f14856a3ae7b99d3a37d3ffaf37fa1363e4d7d6
Patrik Oliveira <patrikro.oliveira@gmail.com>
2024-04-15 18:19:24 -0300

    refactor: :recycle: implementando caso de uso de listar usuarios

--- src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
@@ -1,21 +1,24 @@
 package br.com.alura.codechella.infra.controller;
 
 import br.com.alura.codechella.application.usecases.CriarUsuario;
+import br.com.alura.codechella.application.usecases.ListarUsuarios;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
 import br.com.alura.codechella.infra.controller.dtos.UsuarioRequestDto;
 import br.com.alura.codechella.infra.controller.dtos.UsuarioResponseDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
     private final CriarUsuario criarUsuario;
+    private final ListarUsuarios listarUsuarios;
 
-    public UsuarioController(CriarUsuario criarUsuario) {
+    public UsuarioController(CriarUsuario criarUsuario, ListarUsuarios listarUsuarios) {
         this.criarUsuario = criarUsuario;
+        this.listarUsuarios = listarUsuarios;
     }
 
     @PostMapping
@@ -23,5 +26,13 @@ public class UsuarioController {
         Usuario salvo = criarUsuario.cadastrarUsuario(new Usuario(dto.cpf(), dto.nome(), dto.nascimento(), dto.email()));
 
         return new UsuarioResponseDto(salvo.getCpf(), salvo.getNome(), salvo.getNascimento(), salvo.getEmail());
+    }
+
+    @GetMapping
+    public List<UsuarioResponseDto> listarUsuarios() {
+        return listarUsuarios.obterTodosUsuarios()
+                .stream()
+                .map(u -> new UsuarioResponseDto(u.getCpf(), u.getNome(), u.getNascimento(), u.getEmail()))
+                .collect(Collectors.toList());
     }
 }
